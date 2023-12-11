1311b9e909ff2d0868780692849fa628b8f58fcf
yuriy-kolosov <yuriy.v.kolosov@gmail.com>
2023-12-12 00:41:51 +0300

    Домашнее задание 4 - Начальный коммит

--- Main.java
+++ Main.java
@@ -1,6 +1,4 @@
-public class Main
-{
-    public static void main(String[] args)
+public class Main {    public static void main(String[] args)
     {
         System.out.println("Домашнее задание 4");
     }
